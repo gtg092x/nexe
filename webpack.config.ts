90c2e710b0199c496a2af28c2fb0f992413c5af6
Matthew Drake <mdrake@mediadrake.com>
2025-05-19 13:21:12 -0700

    Update webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,5 @@
 import * as wcp from 'webpack-config-prefabs'
-export = wcp.nodeLibrary(module, {
+export default wcp.nodeLibrary(module, {
   enableTypescript: true,
   entry: './src/fs/patch.ts',
   minimize: false,
